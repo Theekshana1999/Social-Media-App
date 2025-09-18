6221cca59430c14497e89cc3db05cf980b52483b
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-09-18 23:23:39 +0530

    feat(api): implement API class with base URL and user sign-up/login endpoints

---
+++ lib/utils/api.dart
@@ -0,0 +1,6 @@
+class API {
+  final String baseUrl = 'http://192.168.43.20:5000';
+
+  String get registerUrl => '$baseUrl/api/user/sign-up';
+  String get loginUrl => '$baseUrl/api/user/sign-in';
+}

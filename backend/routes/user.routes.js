10962e9a3faf37beb1550667eca8d62251bf1056
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-09-18 22:54:38 +0530

    fix(routes): change sign-in route from GET to POST method

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/user.controller");
 
 router.post("/sign-up",userController.Signup);
 router.post("/update-profile/:id",userController.UpdateProfile);
-router.get("/sign-in",userController.Signin);
+router.post("/sign-in",userController.Signin);
 router.get("/get-all-users",userController.getAllUser);
 router.get("/get-user/:id",userController.getUser);
 
